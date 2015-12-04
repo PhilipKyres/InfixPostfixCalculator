3205c0f4ab7ff154f3690733285b59a735307bd1
pkyres <pkyres@gmail.com>
2015-12-04 01:25:58 -0500

    Finished infix to postfix and postfix to BigDecimal methods. Need to finish the validate method and make testing work with exceptions.

--- 1336260-Calculator/src/main/java/com/philipkyres/calculator/CalculatorAction.java
+++ 1336260-Calculator/src/main/java/com/philipkyres/calculator/CalculatorAction.java
@@ -1,4 +1,5 @@
 package com.philipkyres.calculator; 
+import java.math.BigDecimal;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
@@ -12,16 +13,31 @@ public class CalculatorAction {
 	
 	public CalculatorAction() {}
 	
+	/**
+	 * Validates the infix queue. Throws an IllegalArgumentException if it's invalid
+	 * 
+	 * @param infix queue
+	 * @throws IllegalArgumentException if invalid infix
+	 */
+	public void validate(final Queue<String> infix) throws IllegalArgumentException {
+		if (infix == null)
+			throw new IllegalArgumentException("infix queue must not be null");
+		if (infix.isEmpty())
+			throw new IllegalArgumentException("infix queue must not be empty");
+	}
 	
 	/**
 	 * Logic to convert infix queue to postfix queue
 	 * 
 	 * @param infix queue
 	 * @return postfix queue
+	 * @throws IllegalArgumentException if invalid infix
 	 */
-	public Queue<String> infixToPostfix(final Queue<String> infix) {
+	public Queue<String> infixToPostfix(final Queue<String> infix) throws IllegalArgumentException {
+		validate(infix);
+		
 		Queue<String> postfix = new ArrayDeque<String>();
-		ArrayDeque<String> operator = new ArrayDeque<String>(); //multiply divide plus minus 
+		ArrayDeque<String> operator = new ArrayDeque<String>(); //Operator stack //multiply divide plus minus 
 		
 		for(String s : infix) {		
 			if(isNumeric(s)) { //Numbers strait to postfix
@@ -29,7 +45,7 @@ public class CalculatorAction {
 				continue;
 			} else { //s is operator
 				int sPriority = getPriority(s);
-				if(operator.isEmpty() || getPriority(operator.peek()) == 3) { //If no operators in stack or if previous operator is (
+				if((operator.isEmpty() || getPriority(operator.peek()) == 3) && sPriority != 4) { //If no operators in stack or if previous operator is ( and s is not )
 					operator.push(s); //Push operator in stack
 				continue;
 				} else { //Operator is not empty
@@ -43,25 +59,63 @@ public class CalculatorAction {
 							operator.push(s); //Push operator to stack
 						}
 					} else {
-						while(!operator.isEmpty() && getPriority(s) < getPriority(operator.peek())) {
+						while(!operator.isEmpty() && sPriority <= getPriority(operator.peek()) && getPriority(operator.peek()) != 3) {
 							postfix.add(operator.pop()); //Move stack head to postfix
 						}
+						operator.push(s);
 					}
 				}
 			}
 		}
 		while(!operator.isEmpty()) {
-			postfix.add(operator.pop()); //Move stack to postfix
+			postfix.add(operator.pop()); //Move remaining stack to postfix
 		}
 		return postfix;
 	}
 	
-	public double postfixToDouble(final Queue<String> postfix) {
-		return 0;
+	/**
+	 * Logic to evaluate a postfix queue into a BigDecimal number
+	 * 
+	 * @param postfix queue
+	 * @return evaluation
+	 */
+	public BigDecimal postfixToBigDecimal(final Queue<String> postfix) {
+		ArrayDeque<String> operand = new ArrayDeque<String>(); //Operand stack
+		
+		for(String s : postfix) {
+			if(isNumeric(s)) { //Numbers strait to operand stack
+				operand.push(s);
+				continue;
+			} else { //s is operator
+				BigDecimal val1 = new BigDecimal(operand.pop());
+				BigDecimal val2 = new BigDecimal(operand.pop());
+				BigDecimal retVal;
+				
+				switch (s) {
+					case "+": 
+						retVal = val2.add(val1);
+						break;
+					case "-": 
+						retVal = val2.subtract(val1);
+						break;
+					case "*": 
+						retVal = val2.multiply(val1);
+						break;
+					case "/": 
+						retVal = val2.divide(val1, 3, BigDecimal.ROUND_HALF_UP); //Rounds infinite numbers
+						break;
+					default:
+						throw new IllegalArgumentException("Invalid operand: " + s);
+				}
+				operand.push(retVal.toPlainString());
+			}
+		}
+		
+		return (new BigDecimal(operand.peek())).stripTrailingZeros();
 	}
 	
 	private boolean isNumeric(String s) {
-		return s.matches("\\d+(\\.\\d+)?");
+		return s.matches("(\\d+)?(\\.\\d+)?");
 	}
 	
 	private int getPriority(String s) {
--- 1336260-Calculator/src/test/java/com/philipkyres/test/calculator/CalculatorActionTest.java
+++ 1336260-Calculator/src/test/java/com/philipkyres/test/calculator/CalculatorActionTest.java
@@ -1,96 +1,104 @@
 package com.philipkyres.test.calculator;
 
-import static org.junit.Assert.*;
-
+import java.math.BigDecimal;
 import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Queue;
 
-import org.junit.BeforeClass;
-import org.junit.Rule;
-import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import com.philipkyres.calculator.CalculatorAction;
 import com.philipkyres.test.MethodLogger;
 
-/**
- * Test class for the CalculatorAction class
- * 
- * @author Philip Kyres
- *
- */
-public class CalculatorActionTest {
+import org.junit.Rule;
+import org.junit.Test;
 
+import static org.junit.Assert.*;
+
+/**
+ * A parameterized test class. Requires JUnit 4.11
+ *
+ * @author Philip Kyres
+ */
+@RunWith(Parameterized.class)
+public class CalculatorActionTest {
+	
 	@Rule
 	public MethodLogger methodLogger = new MethodLogger();
+	
+    /**
+     * A static method is required to hold all the data to be tested and the
+     * expected results for each test. This data must be stored in a
+     * two-dimension array. The 'name' attribute of Parameters is a JUnit 4.11
+     * feature
+     *
+     * @return The list of arrays
+     */
+    @Parameters(name = "{index}: infix[{0}] postfix[{1}] result[{2}]")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+        	{"", "", "0"},
+            {"23", "23", "23"},
+            {"23 * 5", "23 5 *", "115"},
+            {"( 23 * 5 )", "23 5 *", "115"},
+            {"23 * ( 54 + 12 )", "23 54 12 + *", "1518"},
+            {"( ( ( ( ( 56.9 * 0.09 ) ) ) ) ) / ( ( 3 - 4.4 ) - ( 79 - .3 ) / ( 3 / 2.4 ) ) / 0.5 - 2", "56.9 0.09 * 3 4.4 - 79 .3 - 3 2.4 / / - / 0.5 / 2 -", "-2.16"},
+            {"2 * 3 - 4 / 5", "2 3 * 4 5 / -", "5.2"},
+            {"10 / 3 + ( 2 * ( 4 - 8.5 / 2.95 ) + 2 )", "10 3 / 2 4 8.5 2.95 / - * 2 + +", "7.571"}
+        });
+    }
+    
+    CalculatorAction calculator;
+    Queue<String> infix;
+    Queue<String> expectedPostfix;
+    BigDecimal expectedResult;
 
-	private final Logger log = LoggerFactory.getLogger(getClass().getName());
-	
-	private static CalculatorAction a;
-	
-	@BeforeClass
-	public static void prepare() {
-		a = new CalculatorAction();
-	}
-	
-	/**
-	 * In this test an infix queue is created and sent through the CalculatorAction infixToPostfix method and checked against the expected queue
-	 * Both queues are converted to arrays before testing so the assert checks only the values
-	 * 
-	 * Test just a number
-	 */
-	@Test
-	public void testInfixToPostfix1() {
-		Queue<String> infix = stringToQueue("23");
-		Queue<String> expected = stringToQueue("23");
-		
-		assertArrayEquals("Expected result array and converted postfix arrays not equal", expected.toArray(), a.infixToPostfix(infix).toArray());	
-	}
-	
-	/**
-	 * In this test an infix queue is created and sent through the CalculatorAction infixToPostfix method and checked against the expected queue
-	 * Both queues are converted to arrays before testing so the assert checks only the values
-	 * 
-	 * Test a simple operation
-	 */
-	@Test
-	public void testInfixToPostfix2() {
-		Queue<String> infix = stringToQueue("23 * 5");
-		Queue<String> expected = stringToQueue("23 5 *");
+    /**
+     * Constructor that receives all the data for each test as defined by a row
+     * in the list of parameters. Converts the infix and expectedPostfix Strings
+     * to Queue objects. Converts the infix queue to a postfix queue. Converts
+     * the expectedResult String to a BigDecimal object.
+     *
+     * @param infix
+     * @param expectedPostfix
+     * @param expectedResult
+     */
+    public CalculatorActionTest(final String infix, final String expectedPostfix, final String expectedResult) {
+        this.calculator = new CalculatorAction();
+        this.infix = stringToQueue(infix);
+        this.expectedPostfix = stringToQueue(expectedPostfix);
+        this.expectedResult = new BigDecimal(expectedResult);
+    }
 
-		assertArrayEquals("Expected result array and converted postfix arrays not equal", expected.toArray(), a.infixToPostfix(infix).toArray());	
-	}
-	
-	/**
-	 * In this test an infix queue is created and sent through the CalculatorAction infixToPostfix method and checked against the expected queue
-	 * Both queues are converted to arrays before testing so the assert checks only the values
-	 * 
-	 * Test with brackets on outsides
-	 */
-	@Test
-	public void testInfixToPostfix3() {
-		Queue<String> infix = stringToQueue("( 23 * 5 )");
-		Queue<String> expected = stringToQueue("23 5 *");
+    /**
+     * Test of validate method, of class CalculatorAction.
+     */
+    @Test
+    public void testValidate() {
+        assertEquals("infix is not valid.", true, true);
+    }
 
-		assertArrayEquals("Expected result array and converted postfix arrays not equal", expected.toArray(), a.infixToPostfix(infix).toArray());	
-	}
-	
-	/**
-	 * In this test an infix queue is created and sent through the CalculatorAction infixToPostfix method and checked against the expected queue
-	 * Both queues are converted to arrays before testing so the assert checks only the values
-	 * 
-	 * Test with operations brackets
-	 */
-	@Test
-	public void testInfixToPostfix4() {
-		Queue<String> infix = stringToQueue("23 * ( 54 + 12 )");
-		Queue<String> expected = stringToQueue("23 54 12 + *");
-		
-		assertArrayEquals("Expected result array and converted postfix arrays not equal", expected.toArray(), a.infixToPostfix(infix).toArray());
-	}
-	
+    /**
+     * Test of infixToPostfix method, of class CalculatorAction.
+     */
+    @Test
+    public void testInfixToPostfix() {
+    	Queue<String> postfix = this.calculator.infixToPostfix(this.infix);
+        assertArrayEquals("Expected postfix array and converted infix to postfix arrays are not equal.", expectedPostfix.toArray(), postfix.toArray());
+    }
+
+    /**
+     * Test of postfixToBigDecimal method, of class CalculatorAction.
+     */
+    @Test
+    public void testPostfixToBigDecimal() {
+    	Queue<String> postfix = this.calculator.infixToPostfix(this.infix);
+        assertEquals("Expected result and calculated result are not equal.", expectedResult, calculator.postfixToBigDecimal(postfix));
+    }
+    
 	private Queue<String> stringToQueue(String s) {
 		Queue<String> queue = new ArrayDeque<String>(Arrays.asList(s.split(" ")));
 		return queue;
